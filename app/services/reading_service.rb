a4ad25e9dbd3b31f76a81366002ca447009ecac3
bob <bozek.wojciech@gmail.com>
2016-08-29 16:43:14 +0000

    Fix fetch method - get first (oldest) notifications, not last (newest)

--- app/services/reading_service.rb
+++ app/services/reading_service.rb
@@ -2,7 +2,7 @@ class ReadingService
   def self.fetch(x, filter={})
     filter.slice!(:email, :cost, :category)
     filter[:unread] = 1
-    notifications = Notification.where(filter).last(x)
+    notifications = Notification.where(filter).first(x)
     notifications.each do |notification|
       notification.update_attribute(:unread, 0)
     end
